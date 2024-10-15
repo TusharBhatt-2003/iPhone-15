5b8ecf6b53f5a4c467e2c3f54a9f8268d1906dc2
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-10-15 11:22:17 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useEffect, useRef } from 'react';
-import LocomotiveScroll from 'locomotive-scroll';
 import 'locomotive-scroll/src/locomotive-scroll.scss'; // Import Locomotive Scroll's styles
 import Navbar from './components/NavBar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
-// import Model from './components/Model';
+import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
 
 const App = () => {
-  const scrollRef = useRef(null); // Create a reference for Locomotive Scroll
-
-  useEffect(() => {
-    const scroll = new LocomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
-      smoothMobile: true,
-    });
-
-    return () => {
-      if (scroll) scroll.destroy(); // Clean up Locomotive Scroll on unmount
-    };
-  }, []);
 
   return (
-    <main data-scroll-container ref={scrollRef} className="bg-black">
+    <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
-      {/* <Model /> */}
+      <Model />
       <Features />
       <HowItWorks />
       <Footer />
